1ed91b76293bc08ce24dc9e7f134e7436bc4ded5
aihamAB12 <aihamab.s10@gmail.com>
2025-01-02 14:58:01 +0200

    delete massage

--- ProjectGammon-ith5rabesh/src/main/java/models/SysData.java
+++ ProjectGammon-ith5rabesh/src/main/java/models/SysData.java
@@ -110,7 +110,6 @@ public class SysData implements Serializable {
         try (FileWriter writer = new FileWriter(path)) {
             writer.write(jsonObject.toJSONString());
             System.out.println("Questions saved successfully to: " + path);
-            JOptionPane.showMessageDialog(this, "Question Added Successfully!");
         } catch (IOException e) {
             e.printStackTrace();
         }
