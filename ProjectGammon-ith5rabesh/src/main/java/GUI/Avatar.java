24e37c05682254e2bf78ccdc92619ad4a78a0544
shahd sabtan <shoshokaa24@gmail.com>
2025-02-02 22:58:51 +0200

    avatars

--- ProjectGammon-ith5rabesh/src/main/java/GUI/Avatar.java
+++ ProjectGammon-ith5rabesh/src/main/java/GUI/Avatar.java
@@ -4,35 +4,33 @@ import javax.swing.ImageIcon;
 
 
 public enum Avatar{
-	//aiham
 	DEFAULT("rabbit1.jpg"),
-	RENARD("rabbit10.jfif"),
-	CHOUETTE("rabbit11.webp"),
+	RENARD("rabbit100.jpg"),
+	CHOUETTE("rabbit101.jpg"),
 	COCHON_D_INDE("rabbit12.jpg"),
 	YEUX_ROUGES("rabbit13.jpg"),
-	ESCARGOTS("rabbit14.jpg"),
-	FLEUR("rabbit15.webp"),
-	GOUTES("rabbit16.webp"),
-	CHATON("rabbit17.webp"),
+	ESCARGOTS("rabbit102.jpg"),
+	FLEUR("rabbit103.jpg"),
+	GOUTES("rabbit104.jpg"),
+	CHATON("rabbit105.jpg"),
 	CHAT_TETE("rabbit18.jpg"),
 	MONGOLFIERERS("rabbit19.jpg"),
-	LUNE("rabbit2.jfif"),
-	LION("rabbit3.jfif"),
+	LUNE("rabbit106.jpg"),
+	LION("rabbit107.jpg"),
 	CHAT_PATAPOUF("rabbit4.jpg"),
-	OISEAU("rabbit5.jfif"),
+	OISEAU("rabiit108.jpg"),
 	ROSE("rabbit6.jpg"),
-	CHEVAL("rabbit7.webp"),
-	LOUP("rabbit8.jfif"),
+	CHEVAL("rabbit109.jpg"),
+	LOUP("rabbitt.jpg"),
 	CHAT_JAUNE("rabbit9.jpg");
 	
 	private final String path;
 	private final ImageIcon icon;
 	
-    Avatar(String fileName) {
-        this.path = "/images/avatars/" + fileName; // Assuming images are in a folder named "avatars"
-        this.icon = new ImageIcon(Avatar.class.getResource(this.path)); // Use getResource
-    }
-
+	Avatar(String path){
+		this.path = ImageAvatar.AVATAR_PATH + path;
+		this.icon = new ImageIcon(this.path);
+	}
 
 	public ImageIcon getIcon() {
 		return icon;
